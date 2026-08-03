b18ae1d2a61abddf08d224d81c4dc3c0f3fa76f4
soufianemchanna <mchanna.soufiane@gmail.com>
2026-08-03 16:53:48 +0200

    Fix schema call for aws credentials

--- src/main/java/com/sosmoch/aws_s3_api/config/S3Config.java
+++ src/main/java/com/sosmoch/aws_s3_api/config/S3Config.java
@@ -11,13 +11,13 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${spring.cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${spring.cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${spring.cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean

0cdbd4372fd5897ae8c2d639c5677dd7c74f86df
soufianemchanna <mchanna.soufiane@gmail.com>
2026-08-19 17:41:21 +0200

    implement default aws credentials for dev environment

--- src/main/java/com/sosmoch/aws_s3_api/config/S3Config.java
+++ src/main/java/com/sosmoch/aws_s3_api/config/S3Config.java
@@ -4,7 +4,9 @@ package com.sosmoch.aws_s3_api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,17 +14,13 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    public S3Client s3Client(){
+    @Bean("s3Client")
+    @Profile("local")
+    public S3Client s3ClientLocal(@Value("${cloud.aws.credentials.access-key}") String accessKey, @Value("${cloud.aws.credentials.secret-key}") String secretKey){
         AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
@@ -30,11 +28,31 @@ public class S3Config {
                 .build();
     }
 
-    @Bean
-    public S3Presigner s3Presigner(){
+    @Bean("s3Presigner")
+    @Profile("local")
+    public S3Presigner s3PresignerLocal(@Value("${cloud.aws.credentials.access-key}") String accessKey, @Value("${cloud.aws.credentials.secret-key}") String secretKey){
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .build();
+    }
+
+    @Bean("s3Client")
+    @Profile("dev")
+    public S3Client s3ClientDev(){
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .build();
+    }
+
+    @Bean("s3Presigner")
+    @Profile("dev")
+    public S3Presigner s3PresignerDev(){
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
                 .build();
     }
 
